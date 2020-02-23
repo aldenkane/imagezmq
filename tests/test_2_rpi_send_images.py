f7e0dcb76a3ef27538c5f623fe239e386bde4f45
aldenkane <akane2@nd.edu>
2020-02-22 20:11:38 -0500

    Edited Send

--- tests/test_2_rpi_send_images.py
+++ tests/test_2_rpi_send_images.py
@@ -18,11 +18,12 @@ import imagezmq
 
 # use either of the formats below to specifiy address of display computer
 # sender = imagezmq.ImageSender(connect_to='tcp://jeff-macbook:5555')
-sender = imagezmq.ImageSender(connect_to='tcp://192.168.1.190:5555')
+sender = imagezmq.ImageSender(connect_to='tcp:/10.254.32.1:5555')
 
 rpi_name = socket.gethostname()  # send RPi hostname with each image
-picam = VideoStream(usePiCamera=True).start()
+cam = cv2.VideoCapture(0)
 time.sleep(2.0)  # allow camera sensor to warm up
+
 while True:  # send images as stream until Ctrl-C
-    image = picam.read()
+    retval, image = cam.read()
     sender.send_image(rpi_name, image)
